25cd4e8fb9431a4f9d0e852f13e694aaaa1fd189
azman-aziz <am911444@gmail.com>
2022-05-29 08:31:50 +0700

    fiks v1

--- index.js
+++ index.js
@@ -17,25 +17,44 @@ function lakukanLooping(arrPegawai) {
   // ! JANGAN DIMODIFIKASI
   let dataYangAkanDilooping = arrPegawai;
 
+  
   /*
     TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
       yang berisi gabungan nama depan dan belakang dari masing masing pegawai
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+      let hasilLooping = "";
+
+ for( const data in dataYangAkanDilooping ){
+    hasilLooping += dataYangAkanDilooping[data].namaDepan + " " + dataYangAkanDilooping[data].namaBelakang ;
+ }
+ 
+  // for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+  //  var hasil  = dataYangAkanDilooping[i].namaDepan + " " + dataYangAkanDilooping[i].namaBelakang;
+  // }
+  
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
-
+  let jumlahPria = 0;
+  for( const data in dataYangAkanDilooping ){
+    if(dataYangAkanDilooping[data].jenisKelamin === "M"){
+      jumlahPria++;
+    }
+  }
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
-      yang berisi jumlah wanita dari masing masing pegawai
+      yang berisi jumlah wanita dari masing masing pegawaizz
   */
-  let jumlahWanita = null;
+  let jumlahWanita = 0;
+  for( const data in dataYangAkanDilooping ){
+    if(dataYangAkanDilooping[data].jenisKelamin === "F"){
+      jumlahWanita++;
+    }
+  }
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -48,7 +67,16 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+  let komentar = "";
+
+  if(jumlahPria > jumlahWanita){
+   komentar = "Jumlah Pria lebih banyak dari Wanita";
+  }
+  else if(jumlahWanita > jumlahPria){
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  }else{
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
